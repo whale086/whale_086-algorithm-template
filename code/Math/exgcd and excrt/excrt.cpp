6a6743d19d6a1d338eeab81221cc5307d2f0892a
whale086 <2312702310@qq.com>
2024-05-16 20:17:22 +0800

    Update excrt.cpp

--- code/Math/exgcd and excrt/excrt.cpp
+++ code/Math/exgcd and excrt/excrt.cpp
@@ -1,4 +1,14 @@
 #define int long long
+int qmul(int a,int b,int mod){
+	int base=0;
+	if(b<0)b=-b,a=-a;
+	while(b){
+		if(b&1)base=(base+a)%mod;
+		b>>=a;
+		a=(a+a)%mod;
+	}
+	return base;
+}
 int exgcd(int a,int b,int &x,int &y){
 	if(!b){x=1;y=0;return a;}
 	int d=exgcd(b,a%b,y,x);
@@ -15,7 +25,7 @@ int excrt(vector<pair<int,int> >q){
 		int x,y;
 		int g=exgcd(M,mod,x,y);
 		if(r%g){
-			q.clear();/////////清空
+			q.clear();//////////清空
 			return -1;
 		}
 		x=(x*r/g%(mod/g));
@@ -23,7 +33,7 @@ int excrt(vector<pair<int,int> >q){
 		
 		int last=M;
 		M=M/g*mod;
-		res=(x*last%M+res)%M;
+		res=(qmul(x,last,M)+res)%M;
 	}
 	q.clear();//////////
 	return res;
