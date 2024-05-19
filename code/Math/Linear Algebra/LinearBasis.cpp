bc01455f25b739e67d397c65495910143bdf396e
whale086 <2312702310@qq.com>
2024-05-19 12:30:51 +0800

    Update LinearBasis.cpp

--- code/Math/Linear Algebra/LinearBasis.cpp
+++ code/Math/Linear Algebra/LinearBasis.cpp
@@ -41,25 +41,23 @@ struct LinearBasis{
 		if(!k)return 0;
 		for(int i=0;i<=60;i++){
 			int now=a[i];
-			for(int j=i-1;~j;j--){
-				if(now&(1ll<<j))now^=a[j];
+			for(int j=i-1;~j;j--){// 从大到小贪心
+				if(now&(1ll<<j))now^=a[j];// 能否清空这一位
 			}
 			if(now)tmp[cnt++]=now;
 		}
-		if(k>=(1ll<<cnt))return -1;
+		if(k>=(1ll<<cnt))return -1;// 不存在第k小
 		for(int i=0;i<cnt;i++){
 			if(k&(1ll<<i))res^=tmp[i];
 		}
 		return res;
 	}
-	int rank(int v){
+	int rank(int v){// 查询x是第几小 查询比x小的数有几个
 		vector<int>st(70);
 		int cnt=0;
 		int res=0;
 		for(int i=0;i<=60;i++){
-			if(a[i]){
-				st[cnt++]=i;
-			}
+			if(a[i])st[cnt++]=i;
 		}
 		for(int i=0;i<cnt;i++){
 			if(v>>st[i]&1)res+=1ll<<i;
@@ -70,7 +68,7 @@ struct LinearBasis{
 	int n;
 	
 	LinearBasis(vector<int>&a):n(a.size()){
-		for(int i=0;i<=n-1;i++)insert(a[i]);
+		for(int i=0;i<n;i++)insert(a[i]);
 	}
 	LinearBasis(){}
 };
